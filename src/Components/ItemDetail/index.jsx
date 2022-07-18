a685ca211ff2d8d35b2afe555a3931422f6c15ab
Jonathan Raices <jonathan.raices@gmail.com>
2022-07-18 13:05:10 -0300

    SicronizarCounterRaices

--- src/Components/ItemDetail/index.jsx
+++ src/Components/ItemDetail/index.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import ItemCount from "../ItemCount";
 
-export const ItemDetail = ({ dataDetail}) => {
+export const ItemDetail = ({ dataDetail }) => {
+  const [goToCart, setGoToCart] = useState(false);
 
   const onAdd = () => {
-    console.log("Agregaste 1 elemento al carrito")
-  }
+    setGoToCart(true);
+  };
 
   return (
     <div>
-         <div className="container">
-            <div className="detail">
-                <div className="content">
-                   <p>{dataDetail.title} </p>
-                  <p>{dataDetail.category}</p>
-                </div>
-                <img className="detail__image" src={dataDetail.image} alt="" />
-            </div>
-            <div>
+      <div className="container">
+        <div className="detail">
+          <div className="content">
+            <p>{dataDetail.title} </p>
+            <p>{dataDetail.category}</p>
+          </div>
+          <img className="detail__image" src={dataDetail.image} alt="" />
+        </div>
+        <div>
+          {goToCart ? (
+            <Link to="/cart"> Terminar compra</Link>
+          ) : (
             <ItemCount initial={1} stock={5} onAdd={onAdd} />
-            </div>
-          </div>  
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default ItemDetail;
